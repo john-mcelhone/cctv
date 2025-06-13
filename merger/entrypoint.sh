394f6e934d0d9e535cc4d4b312b1981a469882c3
john-mcelhone <62488376+john-mcelhone@users.noreply.github.com>
2025-06-12 22:47:20 -0700

    Update entrypoint.sh

--- merger/entrypoint.sh
+++ merger/entrypoint.sh
@@ -1,9 +1,17 @@
+cat > entrypoint.sh <<'EOF'
 #!/bin/bash
 
 echo "Started recording at $(date)"
 
 FILENAME="recording-$(date +%s).mp4"
 
-ffmpeg -i "$RTSP_URL" -t 60 -vcodec copy "$FILENAME"
+ffmpeg -rtsp_transport tcp -i "$RTSP_URL" -t 60 -c copy "$FILENAME"
 
-echo "Done recording. Saved as $FILENAME"
+ftp -inv "$FTP_HOST" <<EOT
+user $FTP_USER $FTP_PASS
+put $FILENAME
+bye
+EOT
+
+echo "Done recording + FTP uploaded $FILENAME"
+EOF
